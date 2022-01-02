4789870e821b948b3e0a820e05fe295fe00cc316
Carlos Gallardo <carlitosgallardo009@gmail.com>
2022-01-01 23:46:49 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
+interface Coin {
+  id: string;
+  name: string;
+  symbol: string;
+  image: string;
+  current_price: number;
+  price_change_percentage_24h: number;
+  total_volume: number;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'angular-crypto';
+export class AppComponent implements OnInit {
+  coins: Coin[] = [];
+  filteredCoints: Coin[] = [];
+  titles: string[] = ['#', 'Coin', 'Price', 'Price Change', '24h Volumen'];
+  searchText = '';
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http
+      .get<Coin[]>(
+        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+      )
+      .subscribe(
+        (data) => {
+          console.log(data);
+          this.coins = data;
+          this.filteredCoints = data;
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  }
+  searchCoin() {
+    this.filteredCoints = this.coins.filter(
+      (coin) =>
+        coin.name.toLowerCase().includes(this.searchText.toLowerCase()) ||
+        coin.symbol.toLowerCase().includes(this.searchText.toLowerCase())
+    );
+  }
 }
